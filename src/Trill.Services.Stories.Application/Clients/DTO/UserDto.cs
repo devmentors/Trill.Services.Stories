104dfae0469326eba7039542f0e70e9deccdcde0
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-08-10 09:20:18 +0200

    user api client

---
+++ src/Trill.Services.Stories.Application/Clients/DTO/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trill.Services.Stories.Application.Clients.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Locked { get; set; }
+    }
+}
