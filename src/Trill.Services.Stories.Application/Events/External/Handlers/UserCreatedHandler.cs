46a8fc6ae369471e56f04c9bef98b053c0fb3ecf
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-08-10 11:17:23 +0200

    user created

--- src/Trill.Services.Stories.Application/Events/External/Handlers/UserCreatedHandler.cs
+++ src/Trill.Services.Stories.Application/Events/External/Handlers/UserCreatedHandler.cs
@@ -1,13 +1,26 @@
 ﻿using System.Threading.Tasks;
 using Convey.CQRS.Events;
+using Trill.Services.Stories.Application.Services;
+using Trill.Services.Stories.Core.Entities;
+using Trill.Services.Stories.Core.Repositories;
 
 namespace Trill.Services.Stories.Application.Events.External.Handlers
 {
     public class UserCreatedHandler : IEventHandler<UserCreated>
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IClock _clock;
+
+        public UserCreatedHandler(IUserRepository userRepository, IClock clock)
+        {
+            _userRepository = userRepository;
+            _clock = clock;
+        }
+
         public async Task HandleAsync(UserCreated @event)
         {
-            await Task.CompletedTask;
+            var user = new User(@event.UserId, @event.Name, _clock.Current());
+            await _userRepository.AddAsync(user);
         }
     }
 }
